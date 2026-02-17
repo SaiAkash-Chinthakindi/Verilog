4c0f3d93979af0b76d87466a7069721f35ca95b3
SaiAkash-Chinthakindi <73585374+SaiAkash-Chinthakindi@users.noreply.github.com>
2026-02-16 23:33:03 -0600

    Update eight_to_three_bit_tb.v

--- Encoders/Basic Encoders/8-to-3 bit/eight_to_three_bit_tb.v
+++ Encoders/Basic Encoders/8-to-3 bit/eight_to_three_bit_tb.v
@@ -10,8 +10,7 @@ module eight_to_three_bit_tb;
  
   initial begin
     $monitor("Time = %0d, A = %b, y = %b, v = %b",$time,A,y,v);
-    A = 8'b0000_0001; #10
-    //$monitor("Time = %0d, A = %0b, y = %0b",$time,A,y);
+    A = 8'b0000_0001; #10;
     for(integer i = 1; i < 8; i = i + 1)begin
        A = A << 1'b1;
        #10;
